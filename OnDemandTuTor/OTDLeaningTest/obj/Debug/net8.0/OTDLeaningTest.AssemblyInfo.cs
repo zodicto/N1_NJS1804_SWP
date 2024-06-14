8c1347fc2c1c4b68683c0736b0a4d9e2e72b2502
leminhtri189 <trilm1892003@gmail.com>
2024-06-14 23:08:56 +0700

    fix bug

--- OnDemandTuTor/OTDLeaningTest/obj/Debug/net8.0/OTDLeaningTest.AssemblyInfo.cs
+++ OnDemandTuTor/OTDLeaningTest/obj/Debug/net8.0/OTDLeaningTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTDLeaningTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c8fc151776f7943a4dd3cfd2915fbac432a76cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64bc20480cac6d9ff1302a902e5bbd9483a4114d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTDLeaningTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTDLeaningTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
