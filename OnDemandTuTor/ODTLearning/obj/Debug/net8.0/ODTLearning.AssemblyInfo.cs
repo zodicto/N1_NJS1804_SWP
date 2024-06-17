a123bef94b2a6efec7d8c17cf5e1c9a3424021fa
leminhtri189 <trilm1892003@gmail.com>
2024-06-17 13:13:43 +0700

    fix api

--- OnDemandTuTor/ODTLearning/obj/Debug/net8.0/ODTLearning.AssemblyInfo.cs
+++ OnDemandTuTor/ODTLearning/obj/Debug/net8.0/ODTLearning.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01cdd9343f740d22e1f947870770f28ce02e36b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7be7518fb8573061ae8e2739185c46a7ae3da77")]
 [assembly: System.Reflection.AssemblyProductAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OnDemandTuTor/OTDLeaningTest/obj/Debug/net8.0/OTDLeaningTest.AssemblyInfo.cs
+++ OnDemandTuTor/OTDLeaningTest/obj/Debug/net8.0/OTDLeaningTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTDLeaningTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01cdd9343f740d22e1f947870770f28ce02e36b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7be7518fb8573061ae8e2739185c46a7ae3da77")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTDLeaningTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTDLeaningTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
