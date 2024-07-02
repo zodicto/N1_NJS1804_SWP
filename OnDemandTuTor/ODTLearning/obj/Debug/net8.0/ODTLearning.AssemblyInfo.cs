9194d407daad995cdc584253d11fac30cf427138
lehaodang189203 <trilmse170573@fpt.edu.vn>
2024-07-02 14:32:46 +0700

    fix confict

--- OnDemandTuTor/ODTLearning/obj/Debug/net8.0/ODTLearning.AssemblyInfo.cs
+++ OnDemandTuTor/ODTLearning/obj/Debug/net8.0/ODTLearning.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d673c69f1226d1192dc8558e09c25a1be2c8915b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ee6c68850c6d3980ef6365d176aca4d38d066c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
