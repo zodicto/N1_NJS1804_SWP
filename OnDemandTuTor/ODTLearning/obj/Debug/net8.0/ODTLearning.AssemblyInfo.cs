4740155ed7a0a7cc0b598d0c627f2c9165934b64
zodicto <minhtan210703@gmail.com>
2024-07-08 18:10:24 +0700

    update get class request and service api

--- OnDemandTuTor/ODTLearning/obj/Debug/net8.0/ODTLearning.AssemblyInfo.cs
+++ OnDemandTuTor/ODTLearning/obj/Debug/net8.0/ODTLearning.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28306b5447af6b2323f4456f24f4dccc61627741")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b84e43166cb752863bd077f10aa7e3afce0289")]
 [assembly: System.Reflection.AssemblyProductAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
