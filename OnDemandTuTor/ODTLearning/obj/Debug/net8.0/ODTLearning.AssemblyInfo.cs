77b4f9741ce1a3dba2108106efc2953197e1cc69
lehaodang189203 <trilmse170573@fpt.edu.vn>
2024-07-11 16:29:15 +0700

    fix conflict

--- OnDemandTuTor/ODTLearning/obj/Debug/net8.0/ODTLearning.AssemblyInfo.cs
+++ OnDemandTuTor/ODTLearning/obj/Debug/net8.0/ODTLearning.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dfa87dbbcc208d32a1148edceb889afd4377e39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ffd656d2f5eac18904e20eb08efa85d84856227")]
 [assembly: System.Reflection.AssemblyProductAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ODTLearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
